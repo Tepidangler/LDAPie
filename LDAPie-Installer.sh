4e953d56f75778dcd9acafb8e900506f4dcc5a44
makiten <dw@angk.org>
2019-02-07 01:06:25 -0600

    refactor: let the installer script move the wordlist to home directory

--- LDAPie-Installer.sh
+++ LDAPie-Installer.sh
@@ -8,6 +8,8 @@ ${GITC} clone https://github.com/Tepidangler/LDAPie.git
 cd LDAPie
 
 ${PYTHON} setup.py bdist_wheel
+mkdir -p ${HOME}/.ldapie
+mv wordlist ${HOME}/.ldapie
 cd dist/
 ${PYTHON} -m pip install *.whl --user
 
